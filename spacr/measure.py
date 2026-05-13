96edd00c705388e4072c26583c75ead0995fbe96
jak18015 <jkellerm@umich.edu>
2026-05-13 12:02:42 -0400

    removed core reservations for measure.py for cluster running

--- spacr/measure.py
+++ spacr/measure.py
@@ -1325,7 +1325,7 @@ def measure_crop(settings):
             else:
                 settings['cytoplasm'] = False
 
-            spacr_cores = int(mp.cpu_count() - 2)  # Reserve 2 cores for other processes
+            spacr_cores = int(mp.cpu_count())  # Reserve no cores for other processes; adjust as needed; on a cluster, you shouldn't need to reserve cores, but on a personal computer you might want to reserve some cores for other processes
             if spacr_cores <= 2:
                 spacr_cores = 1
 
